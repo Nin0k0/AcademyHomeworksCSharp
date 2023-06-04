9d56042f2a35d8f8394fab37fb00bc210974f13f
Nino Gachechiladze <ninogachechiladze@tbcbank.com.ge>
2023-06-04 21:04:57 +0400

    first task done, checking git

---
+++ Homeworks/1_HOMEWORK/1_HOMEWORK/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _1_HOMEWORK
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            #region Task2
+            Console.WriteLine("Choose temperature");
+
+            string temp = Console.ReadLine();
+            if (double.TryParse(temp, out _))
+            {
+                Console.WriteLine(Temperature(double.Parse(temp)));
+
+            }
+            else
+            {
+                Console.WriteLine("Not a Valid temperature");
+            }
+
+            static string Temperature(double temp)
+            {
+                double lowBarier = 0;
+                double highBarier = 30;
+
+                return temp > highBarier ? "It's hot!" :
+                       temp < lowBarier ? "It's cold!" : "Nice Weather!";
+            }
+
+            #endregion
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
