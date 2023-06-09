144cb8910eebf1e000e159d9298fbc63546084a5
Ninoko <nino.gachechiladze.1@btu.edu.ge>
2023-06-09 15:07:16 +0400

    homework4 done

---
+++ Homeworks/Homework_3/Task2/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Write a size of an Array:");
+
+            var strSize = Console.ReadLine();
+
+            if (int.TryParse(strSize, out _))
+            {
+
+                var arraySize = int.Parse(strSize);
+                var strings = new string[arraySize];
+                var ints = new int[arraySize];
+                for (int i = 0; i < 2;)
+                {
+                    if (i == 0)
+                    {
+                        strings = CreateArray<String>(arraySize);
+                        i++;
+                    }
+                    else
+                    {
+                        ints = CreateArray<int>(arraySize);
+                        i++;
+                    }
+                }
+                
+                var resultArray = new string[arraySize];  
+                for (int i = 0;i < arraySize; i++)
+                {
+                    resultArray[i] = $"{strings[i]} {ints[i]}";
+                    
+                }
+
+                Console.WriteLine( string.Join(" ", resultArray));
+
+
+
+
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("Array Size not Valid!");
+            }
+        }
+
+        static T[] CreateArray<T>(int length)
+        {
+            var resultArray = new T[length];
+            var type = typeof(T);
+            var converter = TypeDescriptor.GetConverter(typeof(T));
+            for (int i = 0;i < length;)
+            {
+                Console.WriteLine($"Enter {type} Element # {i+1}");
+
+                var element =Console.ReadLine();
+
+                var converted = (T)converter.ConvertFromString(element);
+
+                resultArray[i] = converted;
+                i++;
+
+            }
+
+            return resultArray;
+        }
+
+
+
+    }
+}
---
+++ Homeworks/Homework_3/Task3/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace Task3
+{
+    internal class Program
+    {
+        static void Main()
+            {
+                Console.WriteLine("Enter the size of the arrays:");
+                int size = int.Parse(Console.ReadLine());
+
+                int[] array1 = new int[size];
+                int[] array2 = new int[size];
+
+                
+                for (int i = 0; i < size; )
+                {
+                try
+                {
+                    Console.WriteLine($"Enter {i+1} element of first array");
+                    array1[i] = int.Parse(Console.ReadLine());
+                    i++;
+                }
+                catch (Exception)
+                {
+
+                    Console.WriteLine("Enter Valid Integer");
+                }
+                    
+                }
+
+                Console.WriteLine("Enter the elements of Array 2:");
+                for (int i = 0; i < size;)
+                {
+      
+                try
+                {
+                    Console.WriteLine($"Enter {i + 1} element of second array");
+                    array2[i] = int.Parse(Console.ReadLine());
+                    i++;
+                }
+                catch (Exception)
+                {
+
+                    Console.WriteLine("Enter Valid Integer");
+                }
+
+            }
+
+                Console.WriteLine("Enter the sorting order (ASC or DESC):");
+                string sortOrder = Console.ReadLine();
+
+                int[] mergedArray = MergeArrays(array1, array2);
+                int[] sortedArray = SortArray(mergedArray, sortOrder);
+
+                Console.WriteLine("Sorted array:");
+                foreach (int element in sortedArray)
+                {
+                    Console.Write(element + " ");
+                }
+
+                Console.WriteLine();
+            }
+
+            static int[] MergeArrays(int[] array1, int[] array2)
+            {
+                int size = array1.Length;
+                int[] mergedArray = new int[size * 2];
+
+                Array.Copy(array1, mergedArray, size);
+                Array.Copy(array2, 0, mergedArray, size, size);
+
+                return mergedArray;
+            }
+
+            static int[] SortArray(int[] array, string sortOrder)
+            {
+                int[] sortedArray = new int[array.Length];
+                Array.Copy(array, sortedArray, array.Length);
+
+                if (sortOrder == "ASC")
+                {
+                    Array.Sort(sortedArray);
+                }
+                else if (sortOrder == "DESC")
+                {
+                    Array.Sort(sortedArray);
+                    Array.Reverse(sortedArray);
+                }
+
+                return sortedArray;
+            }
+        }
+    }
+
