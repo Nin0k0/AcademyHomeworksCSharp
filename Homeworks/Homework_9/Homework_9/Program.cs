eadc86f99d4b8b4f3b0ec20c1113f824d3b9255f
Ninoko <nino.gachechiladze.1@btu.edu.ge>
2023-06-26 13:40:46 +0400

    homework 9 finished

--- Homeworks/Homework_9/Homework_9/Program.cs
+++ Homeworks/Homework_9/Homework_9/Program.cs
@@ -6,7 +6,44 @@ namespace Homework_9
     {
         static void Main()
         {
-            
+            Console.WriteLine("Choose Category:");
+            Console.WriteLine("1. Military");
+            Console.WriteLine("2. Casual");
+            Console.WriteLine("3. Public");
+            Console.WriteLine("4. Sport");
+
+            var choosen =int.TryParse(Console.ReadLine(), out var intValue);
+
+            if (choosen)
+            {
+                switch (intValue)
+                {
+                    case 1:
+                       var newMilitaryVehicle =  MilitaryVehicle.AskQuestions();
+                        newMilitaryVehicle.CreatedVehicleDescription();
+                        newMilitaryVehicle.Description();
+                        break;
+                    case 2:
+                        var newCasualVehicle  = CasualVehicle.AskQuestions();
+                        newCasualVehicle.CreatedVehicleDescription();
+                        newCasualVehicle.Description();
+                        break;
+                    case 3:
+                        var newPublicVehicle = PublicVehicle.AskQuestions();
+                        newPublicVehicle.CreatedVehicleDescription();
+                        newPublicVehicle.Description();
+                        break;
+                    case 4:
+                        var newSportsVehicle = SportVehicle.AskQuestions();
+                        newSportsVehicle.CreatedVehicleDescription();
+                        newSportsVehicle.Description();
+                        break;
+                    default:
+                        Console.WriteLine("Other Category not supported!");
+                        break;
+
+                }
+            }
         }
     }
 }
--- Homeworks/Homework_9/Homework_9/Vehicle.cs
+++ Homeworks/Homework_9/Homework_9/Vehicle.cs
@@ -17,67 +17,151 @@ namespace Homework_9
 
         public abstract void CreatedVehicleDescription();
         public abstract void Description();
+        
     }
 
     public class SportVehicle : Vehicle
     {
-        public  VehicleCategory currentCategory = VehicleCategory.Sport;
+        public const  VehicleCategory CurrentCategory = VehicleCategory.Sport;
         private readonly bool _isF1;
-        public SportVehicle(string name, bool isF1) : base(name, VehicleCategory.Sport)
+        public SportVehicle(string name, bool isF1) : base(name, CurrentCategory)
         {
             _isF1 = isF1;
         }
 
         public override void CreatedVehicleDescription()
         {
-            Console.WriteLine($"this vehicle is the {currentCategory} and it {(_isF1 ? "is" : "is not")} in Formula 1!");
+            Console.WriteLine($"this vehicle is the {CurrentCategory} and it {(_isF1 ? "is" : "is not")} in Formula 1!");
         }
 
         public override void Description() 
         {
-            Console.WriteLine($"this is a Description of {currentCategory} Category!");
+            Console.WriteLine($"this is a Description of {CurrentCategory} Category!");
+        }
+
+        public static SportVehicle AskQuestions()
+        {
+            Console.WriteLine("What's name? ");
+            var name  = Console.ReadLine();
+            Console.WriteLine("is It for F1 Race? y/n ");
+            var f1 = Console.ReadLine();
+            if ( f1 == "y")
+            {
+                return new SportVehicle(name, true);
+            }
+            {
+                return new SportVehicle(name,false);
+            }
         }
     }
 
 
     public class MilitaryVehicle : Vehicle
     {
-        public VehicleCategory currentCategory = VehicleCategory.Military;
+        public const  VehicleCategory CurrentCategory = VehicleCategory.Military;
         private readonly bool _isOffensive;
-        public MilitaryVehicle(string name, bool isOffensive) : base(name, VehicleCategory.Military)
+        public MilitaryVehicle(string name, bool isOffensive) : base(name, CurrentCategory)
         {
             _isOffensive = isOffensive;
         }
 
         public override void CreatedVehicleDescription()
         {
-            Console.WriteLine($"this vehicle is the {currentCategory} and it is for {(_isOffensive ? "Offense" : "Deffense")}!");
+            Console.WriteLine($"this vehicle is the {CurrentCategory} and it is for {(_isOffensive ? "Offense" : "Deffense")}!");
         }
 
         public override void Description()
         {
-            Console.WriteLine($"this is a Description of {currentCategory} Category!");
+            Console.WriteLine($"this is a Description of {CurrentCategory} Category!");
+        }
+
+
+        public static MilitaryVehicle AskQuestions()
+        {
+            Console.WriteLine("What's name? ");
+            var name = Console.ReadLine();
+            Console.WriteLine("is It Offensive vehicle? y/n ");
+            var f1 = Console.ReadLine();
+            if (f1 == "y")
+            {
+                return new MilitaryVehicle(name, true);
+            }
+            {
+                return new MilitaryVehicle(name, false);
+            }
         }
     }
 
     public class CasualVehicle : Vehicle
     {
-        public VehicleCategory currentCategory = VehicleCategory.Casual;
+        public const VehicleCategory CurrentCategory = VehicleCategory.Casual;
         private readonly bool _isForFamily;
-        public CasualVehicle(string name, bool isForFamily) : base(name, VehicleCategory.Casual)
+        public CasualVehicle(string name, bool isForFamily) : base(name, CurrentCategory)
         {
             _isForFamily = isForFamily;
         }
 
         public override void CreatedVehicleDescription()
         {
-            Console.WriteLine($"this vehicle is the {currentCategory} and it is for {(_isForFamily ? "Family people" : "Single people")}!");
+            Console.WriteLine($"this vehicle is the {CurrentCategory} and it is for {(_isForFamily ? "Family people" : "Single people")}!");
         }
 
         public override void Description()
         {
-            Console.WriteLine($"this is a Description of {currentCategory} Category!");
+            Console.WriteLine($"this is a Description of {CurrentCategory} Category!");
         }
+
+        public static CasualVehicle AskQuestions()
+        {
+            Console.WriteLine("What's name? ");
+            var name = Console.ReadLine();
+            Console.WriteLine("is It for family usage? y/n ");
+            var f1 = Console.ReadLine();
+            if (f1 == "y")
+            {
+                return new CasualVehicle(name, true);
+            }
+            {
+                return new CasualVehicle(name, false);
+            }
+        }
+    }
+
+    public class PublicVehicle : Vehicle
+    {
+        public const VehicleCategory CurrentCategory = VehicleCategory.Casual;
+        private readonly bool _canTakeMoreThan20;
+        public PublicVehicle(string name, bool canTakeMoreThan20) : base(name, CurrentCategory)
+        {
+            _canTakeMoreThan20 = canTakeMoreThan20;
+        }
+
+        public override void CreatedVehicleDescription()
+        {
+            Console.WriteLine($"this vehicle is the {CurrentCategory} and it is for {(_canTakeMoreThan20 ? "More than" : "Less than")} 20 people!");
+        }
+
+        public override void Description()
+        {
+            Console.WriteLine($"this is a Description of {CurrentCategory} Category!");
+        }
+
+        public static PublicVehicle AskQuestions()
+        {
+            Console.WriteLine("What's name? ");
+            var name = Console.ReadLine();
+            Console.WriteLine("Can it take more than 20 people? y/n ");
+            var f1 = Console.ReadLine();
+            if (f1 == "y")
+            {
+                return new PublicVehicle(name, true);
+            }
+            {
+                return new PublicVehicle(name, false);
+            }
+        }
+
+
     }
 
 
